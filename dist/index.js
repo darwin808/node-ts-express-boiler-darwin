8ed44ca191377a1360f296302423492dec60c4ae
darwin apolinario <darwin.apolinario12>
2022-01-22 18:21:16 +0800

    initial release

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+console.log(123123123);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoute from "./Users";
+import postsRoute from "./Posts/indext";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+app.use("/api", userRoute);
+app.use("/api", postsRoute);
+app.get("/", (req: Request, res: Response) => {
+  console.log(req);
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
